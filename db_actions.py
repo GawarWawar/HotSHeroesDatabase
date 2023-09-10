20eec977ba46fb2be417d19e0464272e3289a351
GawarWawar <gawarwawar@gmail.com>
2023-09-10 17:56:38 +0300

    db_actions.py
    - class InstanceActions:
    -- added init_subclass to it,
    --- it is writing table as a parametr to class
    -- modified get_id_by_name:
    --- to return id, not create id parametr
    --- to show return classes
    -- added add_row_to_table method:
    --- adds row to the self.table of the class
    --- it needs to be provided with columns and values as a tuples
    -- added delete_row_from_table_by_id:
    --- deletes from self.table row by its id
    --- needs to be provided with name of the column where id is contaminated,
    ---- doesnt have base value
    -- created sub class Hero:
    --- table="heroes"
    ---supports all of the method that parent class provides
    
    - + provided small example below to test stuff
    
    Next step: make Role subclass of  InstanceActions

--- db_actions.py
+++ db_actions.py
@@ -4,11 +4,17 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from psycopg2.errors import DuplicateTable, UniqueViolation
 
 class InstanceActions ():
+    def __init_subclass__(
+        cls,
+        table,
+        *args, **kwargs
+    ) -> None:
+        cls.table = table
+    
     def get_id_by_name(
         self,
-        table,
-        id = None,
-        ) -> None:
+        id:int = None,
+        ) -> int|None:
         if id == None:
             with psycopg2.connect(
                 database = "postgres", user = "postgres", 
@@ -17,18 +23,83 @@ class InstanceActions ():
             ) as conn:
                 with conn.cursor() as cur:
                     cur.execute(
-                        f"""SELECT * FROM {table} 
+                        f"""SELECT * FROM {self.table} 
                         WHERE name=%s
                         """, (self.name,)
                     )
                     element = cur.fetchone()
-                    if len(element) == 0:
-                        self.id = None
-                    else:
-                        print(element, element[0])
+                    try:
+                        if len(element) == 0:
+                            id = None
+                        else:
+                            id = element[0]
+                        return id
+                    except TypeError:
+                        id = None
+                        return id
+    
+    def add_row_to_table(
+        self,
+        columns: tuple,
+        values: tuple
+    ): 
+        if self.id == None:
+            with psycopg2.connect(
+                database = "postgres", user = "postgres", 
+                host= 'localhost', password = "123qwedsacxz",
+                port = 5432
+            ) as conn:
+                with conn.cursor() as cur:
+                    try:
+                        values_representation = []
+                        for i in range(len(values)):
+                            values_representation.append("%s")
+                        
+                        cur.execute(
+                            f"""INSERT INTO {self.table} ({', '.join(columns)}) 
+                            VALUES ({', '.join(['%s'] * len(values))})
+                            RETURNING *
+                            """, values
+                        )
+                    except UniqueViolation:
+                        conn.rollback()
+                        cur.execute(
+                            f"""SELECT * FROM {self.table} 
+                            WHERE name=%s
+                            """, (self.name,)
+                        )
+                        element = cur.fetchone()
+                        return f"UniqueViolation was raised with {element}"
+                    else:     
+                        element = cur.fetchone()
+                        conn.commit()
                         self.id = element[0]
+                        return f"Instance in {self.table} created sucsessfully with id {element[0]}"
+        else:
+            return f"Instance in {self.table} already exist with id {self.id}"
         
-
+    def delete_row_from_table_by_id (
+        self, 
+        column_with_id_name: str
+    ):
+        with psycopg2.connect(
+            database = "postgres", user = "postgres", 
+            host= 'localhost', password = "123qwedsacxz",
+            port = 5432
+        ) as conn:
+            if self.id != None:
+                with conn.cursor() as cur:
+                    cur.execute(
+                        f"""DELETE FROM {self.table} CASCADE
+                        WHERE {column_with_id_name}= %s
+                        RETURNING *
+                        """, (self.id,)
+                    )
+                    element = cur.fetchone()
+                    self.id = None
+                    return f"Instance in table {self.table} with id {element[0]} deleted"
+            else:
+                return f"There no matching instance for this parametrs in table {self.table} yet"
 
 
 class Role ():
@@ -52,11 +123,13 @@ class Role ():
                         """, (self.name,)
                     )
                     element = cur.fetchone()
-                    if len(element) == 0:
+                    try:
+                        if len(element) == 0:
+                            self.id = None
+                        else:
+                            self.id = element[0]
+                    except TypeError:
                         self.id = None
-                    else:
-                        print(element, element[0])
-                        self.id = element[0]
     
     def add_role_to_db (self):
         if self.id == None:
@@ -102,47 +175,39 @@ class Role ():
                     return f"Role with id {element[0]} deleted"
             else:
                 return "This role doesnt exist yet"
-                    
-                    
 
-
-class Hero ():
+                    
+class Hero (InstanceActions, table = "heroes"):
     def __init__(
         self,
         name,
         role: Role,
         id = None
     ) -> None:
-        self.name = name,
+        self.name = name
         self.role_id = role.id
-        if id == None:
-            with psycopg2.connect(
-                database = "postgres", user = "postgres", 
-                host= 'localhost', password = "123qwedsacxz",
-                port = 5432
-            ) as conn:
-                with conn.cursor() as cur:
-                    cur.execute(
-                        """SELECT * FROM heroes 
-                        WHERE name=%s
-                        """, (self.name,)
-                    )
-                    element = cur.fetchone()
-                    if len(element) == 0:
-                        self.id = None
-                    else:
-                        self.id = element[0]
+        self.id = self.get_id_by_name(id)
     
-    def add_player_to_db (self):
-        pass
+    def add_row_to_table(self):
+        columns = ("name", "role_id")
+        values = (self.name, self.role_id)
+        return super().add_row_to_table(columns, values)
+    
+    def delete_row_from_table_by_id(self):
+        column_with_id_name = "id"
+        return super().delete_row_from_table_by_id(column_with_id_name)
+    
         
         
 if __name__ == "__main__":
     
-    role_to_add = Role("Assasin")
-    print(role_to_add.add_role_to_db())
-    print(role_to_add.delete_role_from_db())
-    print(role_to_add.add_role_to_db())
+    role = Role("Assasin")
+    print(role.add_role_to_db())
+    hero = Hero("Jaina", role)
+    print(hero.add_row_to_table())
+    print(hero.delete_row_from_table_by_id())
+    print(hero.add_row_to_table())
+    print(hero)
 
 # cur.execute("""SELECT * FROM roles""")
 # colnames = [desc[0] for desc in cur.description]
