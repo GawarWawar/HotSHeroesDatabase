d2ab277b7df00ed42f09bed6cc19a4cc955b216d
GawarWawar <gawarwawar@gmail.com>
2023-10-06 22:41:02 +0300

    instance_action changes
    - InstanceActions -> InstanceObject
    - __init_subclass__:
    -- added cls.table_columns_info with name and dtype of all columns
    - added __init__:
    -- creates attributes of the class based on provided args or kwargs for each column of the table ->
    - deletedd __init__ of RolesInstance and HeroesInstance,
    -- doesnt need to make special __init__ for new classes anymore
    - to_dict changed
    - add to_list, to_tuple

--- database_managing/instance_actions.py
+++ database_managing/instance_actions.py
@@ -2,19 +2,93 @@ import psycopg2
 from psycopg2 import sql
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from psycopg2.errors import DuplicateTable, UniqueViolation
+from psycopg2.extensions import Column
 
-class InstanceActions ():
+class InstanceObject ():
     def __init_subclass__(
         cls,
         table,
         *args, **kwargs
     ) -> None:
-        cls.table = table
+        cls.table = table 
+        dictionary_with_columns_info = {}
+        with psycopg2.connect(
+            database = "postgres", user = "postgres", 
+            host= 'localhost', password = "123qwedsacxz",
+            port = 5432
+        ) as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    f"""
+                        SELECT COLUMN_NAME, DATA_TYPE
+                        FROM information_schema.columns
+                        WHERE table_name = '{cls.table}' 
+                    """
+                )
+                columns_info = cur.fetchall()
+                for column_number, column in enumerate(columns_info):
+                    dictionary_with_columns_info.update(
+                        {
+                            column_number:{
+                                "name": column[0],
+                                "dtype": column[1]
+                            }
+                        }
+                    )               
+        cls.table_columns_info = dictionary_with_columns_info
+    
+    def __init__(
+        self,
+        *args, **kwargs
+    ) -> None:
+        """
+            Initialize an instance of the class with provided values.
+
+            This constructor accepts either positional arguments (*args) or keyword arguments (**kwargs) to initialize
+            instance attributes based on the class's predefined table columns. If both args and kwargs are provided, it raises
+            an AttributeError. If args are used, they should match the order of columns in the table_columns_info attribute.
+            If kwargs are used, they should match the column names.
+
+            Args:
+                *args: Positional arguments to initialize instance attributes (should match the order of table columns).
+                **kwargs: Keyword arguments to initialize instance attributes (should match column names).
+
+            Raises:
+                AttributeError: If both args and kwargs are provided.
+        """
+        # Initialize an empty list to store attribute values
+        value_list = [] 
+        
+        # Check if both args and kwargs are provided, and raise an error if so
+        if args and kwargs:
+            raise AttributeError("Function can accept args or kwargs. Choose only one method to use")
+        
+        # Initialize the value_list based on the provided args or kwargs
+        if args:
+            value_list = args        
+        elif kwargs:
+            for column_name in kwargs:
+                add_count = 0
+                for column in self.table_columns_info:
+                    if column_name == self.table_columns_info[column]["name"]:
+                        value_list.append(kwargs.get(column_name))
+                        add_count += 1
+                if add_count == 0:
+                    value_list.append(None)
+        else:
+            # If neither args nor kwargs are provided, initialize with None values
+            for column_position in range(len(self.table_columns_info)):
+                value_list.append(None)
+        
+        # Create instance attributes based on column names
+        for column_position, column in enumerate(self.table_columns_info):
+            column_name = self.table_columns_info[column]["name"]
+            setattr(self, column_name, value_list[column_position])     
     
     def get_id_by_name(
         self,
         id:int = None,
-        ) -> int|None:
+    ) -> int|None:
         if id == None:
             with psycopg2.connect(
                 database = "postgres", user = "postgres", 
@@ -37,28 +111,7 @@ class InstanceActions ():
                 except TypeError:
                     id = None
                     return id
-    
-    def to_dict (self):
-        with psycopg2.connect(
-                database = "postgres", user = "postgres", 
-                host= 'localhost', password = "123qwedsacxz",
-                port = 5432
-            ) as conn:
-                with conn.cursor() as cur:
-                    cur.execute(f"""SELECT * FROM {self.table}
-                                WHERE id=%s
-                                """, (self.id,))
-                    column_names = [desc[0] for desc in cur.description]
-                    element = cur.fetchone()
-                dict_to_return = {}
-                for column_count, column in enumerate(column_names):
-                    dict_to_return.update(
-                        {
-                            column: element[column_count]
-                        }
-                    )
-                return dict_to_return
-    
+        
     def add_row_to_table(
         self,
         columns: tuple,
@@ -121,55 +174,40 @@ class InstanceActions ():
                 return f"Instance in table {self.table} with id {element[0]} deleted"
             else:
                 return f"There no matching instance for this parametrs in table {self.table} yet"
-
-class RolesInstance (InstanceActions, table = "roles"):
-    def __init__(
+    
+    def to_dict (
         self,
-        name,
-        id = None
-    ) -> None:
-        self.name = name
-        self.id = self.get_id_by_name(id)
+    ):
+        return self.__dict__
     
-    def add_row_to_table(self):
-        columns = ("name",)
-        values = (self.name,)
-        return super().add_row_to_table(columns, values)
+    def to_list (
+        self
+    ):
+        list_to_return = []
+        for key, item in self.__dict__.items():
+            list_to_return.append(item)
+        return list_to_return
     
-    def delete_row_from_table_by_id(self):
-        column_with_id_name = "id"
-        return super().delete_row_from_table_by_id(column_with_id_name)
+    def to_tuple (
+        self
+    ):
+        list_to_return = []
+        for key, item in self.__dict__.items():
+            list_to_return.append(item)
+        return tuple(list_to_return)
+    
+    # def __str__(self) -> str:
+    #     return self.__dict__
 
+class RolesInstance (InstanceObject, table = "roles"):
+    pass
                  
-class HeroesInstance (InstanceActions, table = "heroes"):
-    def __init__(
-        self,
-        name,
-        role_id: int,
-        id = None
-    ) -> None:
-        self.name = name
-        self.role_id = role_id
-        self.id = self.get_id_by_name(id)
+class HeroesInstance (InstanceObject, table = "heroes"):
+   pass
     
-    def add_row_to_table(self):
-        columns = ("name", "role_id")
-        values = (self.name, self.role_id)
-        return super().add_row_to_table(columns, values)
-    
-    def delete_row_from_table_by_id(self):
-        column_with_id_name = "id"
-        return super().delete_row_from_table_by_id(column_with_id_name)
-    
-        
         
 if __name__ == "__main__":
-    
-    role0 = RolesInstance("Assasin")
-    print(role0.add_row_to_table())
-    print(RolesInstance("Bruiser").add_row_to_table())
-    print(RolesInstance("Tank").add_row_to_table())
-    print(RolesInstance("Heal").add_row_to_table())
-    hero = HeroesInstance("Jaina", role0.id)
-    print(hero.add_row_to_table())
-    print(hero.to_dict())
+    asasin = RolesInstance(1, "Assasin")
+    print(asasin.table_columns_info)
+    print(HeroesInstance(15, "jaina", 1).to_list())
+
